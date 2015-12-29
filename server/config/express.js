719a7ed30165f76c58f8ad7a8388e83691497a8b
Pavlo Kuzhel <pavlick@gmail.com>
2015-12-29 17:44:54 -0500

    Work on a few misc issues
    I found that font awesome doesn't load because of its CSS declaration
    Changing the cert file which has to be loaded for iOS webview to work
    And we do not need a template renderer, so got rid of EJS but small tweak is needed to complete this.
    
    If any more issues arise, I will attempt to fix by closing this issue
    
    Fixes #53

--- server/config/express.js
+++ server/config/express.js
@@ -19,8 +19,13 @@ var passport = require('passport');
 module.exports = function(app) {
   var env = app.get('env');
 
+  // if using plain HTML, no need to set view engine
+  // as long as we only need to load the index.html
+  // however we have many calls that use res.render vs res.json
+  // thus keeping ejs
   app.set('views', config.root + '/server/views');
-  app.set('view engine', 'html');
+  app.set('view engine', 'ejs');
+
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
